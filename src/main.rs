d221a64618e89b65edd408ce64b87ab0e7c6990d
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-10-17 01:14:23 +0200

    Use interrupts for rx.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ mod fmt;
 
 mod modem;
 
-use core::sync::atomic::{compiler_fence, fence, Ordering};
-use core::{slice, str};
+use core::mem::MaybeUninit;
+use core::slice;
 
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_nrf::{pac, Peripherals};
-use embassy_time::{Duration, Timer};
+use embassy_nrf::gpio::{AnyPin, Level, Output, OutputDrive, Pin};
+use embassy_nrf::pac;
+use embassy_time::Timer;
 use futures::future::pending;
 use static_cell::make_static;
 use {defmt_rtt as _, panic_probe as _};
@@ -25,17 +25,32 @@ async fn modem_task(runner: Runner<'static>) -> ! {
     runner.run().await
 }
 
+#[embassy_executor::task]
+async fn blink_task(pin: AnyPin) {
+    let mut led = Output::new(pin, Level::Low, OutputDrive::Standard);
+    loop {
+        led.set_high();
+        Timer::after_millis(100).await;
+        led.set_low();
+        Timer::after_millis(100).await;
+    }
+}
+
+extern "C" {
+    static __start_ipc: u8;
+    static __end_ipc: u8;
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
 
     info!("Hello World!");
 
-    let mut led = Output::new(p.P0_02, Level::Low, OutputDrive::Standard);
-    led.set_high();
+    unwrap!(spawner.spawn(blink_task(p.P0_02.degrade())));
 
     let spu = unsafe { &*pac::SPU::ptr() };
-    info!("Setting all RAM as nonsecure...");
+    info!("Setting IPC RAM as nonsecure...");
     for i in 0..32 {
         spu.ramregion[i].perm.write(|w| {
             w.execute().set_bit();
@@ -50,19 +65,22 @@ async fn main(_spawner: Spawner) {
     spu.periphid[42].perm.write(|w| w.secattr().non_secure());
     info!("spu ipc perm: {:08x}", spu.periphid[42].perm.read().bits());
 
+    let ipc_mem = unsafe {
+        let ipc_start = &__start_ipc as *const u8 as *mut MaybeUninit<u8>;
+        let ipc_end = &__end_ipc as *const u8 as *mut MaybeUninit<u8>;
+        let ipc_len = ipc_end.offset_from(ipc_start) as usize;
+        slice::from_raw_parts_mut(ipc_start, ipc_len)
+    };
+
     let state = make_static!(State::new());
-    let (dev, mut control, runner) = modem::new(state).await;
+    let (dev, mut control, runner) = modem::new(state, ipc_mem).await;
+    unwrap!(spawner.spawn(modem_task(runner)));
 
     let mut buf = *b"AT+CFUN?";
 
     control.send_at(&mut buf);
+    info!("AT command sent");
 
-    loop {
-        compiler_fence(Ordering::SeqCst);
-        control.process_ctrl();
-        control.process_data();
-    }
-
-    info!("handshake complete, modem is alive");
+    // needed so we don't deallocate `buf`, because we're not copying it into IPC MEM yet.
     pending::<()>().await;
 }
--- src/modem.rs
+++ src/modem.rs
@@ -1,35 +1,104 @@
+use core::future::poll_fn;
 use core::marker::PhantomData;
 use core::mem::{self, MaybeUninit};
 use core::sync::atomic::{compiler_fence, fence, Ordering};
+use core::task::Poll;
 use core::{slice, str};
 
 use embassy_net_driver_channel as ch;
-use embassy_nrf::pac;
+use embassy_nrf::interrupt::InterruptExt;
+use embassy_nrf::{interrupt, pac};
+use embassy_sync::waitqueue::AtomicWaker;
 
+const RX_SIZE: usize = 4096;
 const MTU: usize = 1500;
 
 pub type NetDriver<'a> = ch::Device<'a, MTU>;
 
-pub async fn new<'a>(state: &'a mut State) -> (NetDriver<'a>, Control<'a>, Runner<'a>) {
+static WAKER: AtomicWaker = AtomicWaker::new();
+#[interrupt]
+fn IPC() {
+    let ipc = unsafe { &*pac::ns::IPC::ptr() };
+
+    info!("irq");
+
+    ipc.inten.write(|w| w);
+    WAKER.wake();
+}
+
+struct Allocator<'a> {
+    start: *mut u8,
+    end: *mut u8,
+    _phantom: PhantomData<&'a mut u8>,
+}
+
+impl<'a> Allocator<'a> {
+    fn alloc_bytes(&mut self, size: usize) -> &'a mut [MaybeUninit<u8>] {
+        // safety: both pointers come from the same allocation.
+        let available_size = unsafe { self.end.offset_from(self.start) } as usize;
+        if size > available_size {
+            panic!("out of memory")
+        }
+
+        // safety: we've checked above this doesn't go out of bounds.
+        let p = self.start;
+        self.start = unsafe { p.add(size) };
+
+        // safety: we've checked the pointer is in-bounds.
+        unsafe { slice::from_raw_parts_mut(p as *mut _, size) }
+    }
+
+    fn alloc<T>(&mut self) -> &'a mut MaybeUninit<T> {
+        let align = mem::align_of::<T>();
+        let size = mem::size_of::<T>();
+
+        let align_size = match (self.start as usize) % align {
+            0 => 0,
+            n => align - n,
+        };
+
+        // safety: both pointers come from the same allocation.
+        let available_size = unsafe { self.end.offset_from(self.start) } as usize;
+        if align_size + size > available_size {
+            panic!("out of memory")
+        }
+
+        // safety: we've checked above this doesn't go out of bounds.
+        let p = unsafe { self.start.add(align_size) };
+        self.start = unsafe { p.add(size) };
+
+        // safety: we've checked the pointer is aligned and in-bounds.
+        unsafe { &mut *(p as *mut _) }
+    }
+}
+
+pub async fn new<'a>(
+    state: &'a mut State,
+    shmem: &'a mut [MaybeUninit<u8>],
+) -> (NetDriver<'a>, Control<'a>, Runner<'a>) {
+    let shmem_len = shmem.len();
+    let shmem_ptr = shmem.as_mut_ptr() as *mut u8;
+    let mut alloc = Allocator {
+        start: shmem_ptr,
+        end: unsafe { shmem_ptr.add(shmem_len) },
+        _phantom: PhantomData,
+    };
+
     let ipc = unsafe { &*pac::ns::IPC::ptr() };
     let power = unsafe { &*pac::POWER::ptr() };
 
-    let mut cb = unsafe {
-        CONTROL_BLOCK.write(mem::zeroed());
-        CONTROL_BLOCK.assume_init_mut()
-    };
+    let cb: &mut ControlBlock = alloc.alloc().write(unsafe { mem::zeroed() });
+    let rx = alloc.alloc_bytes(RX_SIZE);
 
-    unsafe {
-        cb.flags[2] = 1;
-        cb.rx_base = &mut RX as *mut _ as _;
-        cb.rx_size = RX_SIZE;
-        cb.p_ctrl_list = &mut cb.ctrl_list;
-        cb.p_data_list = &mut cb.data_list;
-        cb.p_modem_descriptor = &mut cb.modem_descriptor;
-        cb.p_trace = &mut cb.trace;
-        cb.ctrl_list.len = 8;
-        cb.data_list.len = 8;
-    }
+    cb.flags[2] = 1;
+    cb.rx_base = rx.as_mut_ptr() as _;
+    cb.rx_size = RX_SIZE;
+    cb.p_ctrl_list = &mut cb.ctrl_list;
+    cb.p_data_list = &mut cb.data_list;
+    cb.p_modem_descriptor = &mut cb.modem_descriptor;
+    cb.p_trace = &mut cb.trace;
+    cb.ctrl_list.len = 8;
+    cb.data_list.len = 8;
 
     info!("rx_base {:x}", cb.rx_base);
     info!("p_ctrl_list {:x}", cb.p_ctrl_list);
@@ -38,10 +107,11 @@ pub async fn new<'a>(state: &'a mut State) -> (NetDriver<'a>, Control<'a>, Runne
     info!("ctrl_msgs {:x}", &mut cb.ctrl_msgs as *mut _);
     info!("data_msgs {:x}", &mut cb.data_msgs as *mut _);
 
-    ipc.inten.write(|w| unsafe { w.bits(0xFF) });
+    //ipc.inten.write(|w| unsafe { w.bits(0xFF) });
     ipc.gpmem[0].write(|w| unsafe { w.bits(cb as *mut _ as u32) });
     ipc.gpmem[1].write(|w| unsafe { w.bits(0) });
 
+    // connect task/event i to channel i
     for i in 0..8 {
         ipc.send_cnf[i].write(|w| unsafe { w.bits(1 << i) });
         ipc.receive_cnf[i].write(|w| unsafe { w.bits(1 << i) });
@@ -58,13 +128,11 @@ pub async fn new<'a>(state: &'a mut State) -> (NetDriver<'a>, Control<'a>, Runne
 
     loop {
         compiler_fence(Ordering::SeqCst);
-
-        let int = ipc.intpend.read();
-        if int.receive0().is_pending() {
+        if ipc.events_receive[0].read().bits() != 0 {
             info!("IPC 0");
             ipc.events_receive[0].reset();
         }
-        if int.receive2().is_pending() {
+        if ipc.events_receive[2].read().bits() != 0 {
             info!("IPC 2");
             ipc.events_receive[2].reset();
 
@@ -76,19 +144,15 @@ pub async fn new<'a>(state: &'a mut State) -> (NetDriver<'a>, Control<'a>, Runne
             info!("data_list: {:08x}", cb.modem_descriptor.data_list);
             break;
         }
-        if int.receive4().is_pending() {
-            info!("IPC 4");
-            ipc.events_receive[4].reset();
-        }
-        if int.receive6().is_pending() {
-            info!("IPC 6");
-            ipc.events_receive[6].reset();
-        }
-        if int.receive7().is_pending() {
-            info!("IPC 7");
-            ipc.events_receive[7].reset();
-        }
     }
+
+    ipc.intenset.write(|w| {
+        w.receive0().set_bit();
+        w.receive2().set_bit();
+        w.receive4().set_bit();
+        w
+    });
+    unsafe { interrupt::IPC.enable() };
 
     assert_eq!(cb.modem_descriptor.version, 1);
     let rx_ctrl = unsafe { &mut *cb.modem_descriptor.ctrl_list };
@@ -97,17 +161,22 @@ pub async fn new<'a>(state: &'a mut State) -> (NetDriver<'a>, Control<'a>, Runne
     info!("rx_data_len: {}", rx_data.len);
 
     let control = Control {
+        cb,
         rx_ctrl,
         rx_data,
         tx_seq_no: 0,
-        rx_seq_no: 0,
         _phantom: PhantomData,
     };
 
     let (ch_runner, device) = ch::new(&mut state.ch, ch::driver::HardwareAddress::Ethernet([0; 6]));
     let state_ch = ch_runner.state_runner();
 
-    let mut runner = Runner { ch: ch_runner };
+    let runner = Runner {
+        ch: ch_runner,
+        rx_ctrl,
+        rx_data,
+        rx_seq_no: 0,
+    };
 
     (device, control, runner)
 }
@@ -124,33 +193,60 @@ impl State {
 
 pub struct Runner<'a> {
     ch: ch::Runner<'a, MTU>,
+    rx_ctrl: *mut List,
+    rx_data: *mut List,
+    rx_seq_no: u16,
 }
 
 pub struct Control<'a> {
+    cb: &'a mut ControlBlock,
     rx_ctrl: *mut List,
     rx_data: *mut List,
 
-    rx_seq_no: u16,
     tx_seq_no: u16,
 
     _phantom: PhantomData<&'a mut ()>,
 }
 
 impl<'a> Runner<'a> {
-    pub async fn run(self) -> ! {
-        todo!()
-    }
-}
-
-impl<'a> Control<'a> {
-    pub fn process_ctrl(&mut self) {
-        let list = unsafe { &mut *self.rx_ctrl };
-        self.process(list);
+    pub async fn run(mut self) -> ! {
+        poll_fn(|cx| {
+            WAKER.register(cx.waker());
+            self.poll();
+            Poll::Pending
+        })
+        .await
     }
 
-    pub fn process_data(&mut self) {
-        let list = unsafe { &mut *self.rx_data };
-        self.process(list);
+    fn poll(&mut self) {
+        info!("poll!");
+        let ipc = unsafe { &*pac::ns::IPC::ptr() };
+
+        if ipc.events_receive[0].read().bits() != 0 {
+            ipc.events_receive[0].reset();
+            info!("ipc 0");
+        }
+
+        if ipc.events_receive[2].read().bits() != 0 {
+            ipc.events_receive[2].reset();
+            info!("ipc 2");
+            let list = unsafe { &mut *self.rx_ctrl };
+            self.process(list);
+        }
+
+        if ipc.events_receive[4].read().bits() != 0 {
+            ipc.events_receive[4].reset();
+            info!("ipc 4");
+            let list = unsafe { &mut *self.rx_data };
+            self.process(list);
+        }
+
+        ipc.intenset.write(|w| {
+            w.receive0().set_bit();
+            w.receive2().set_bit();
+            w.receive4().set_bit();
+            w
+        });
     }
 
     fn process(&mut self, list: &mut List) {
@@ -169,11 +265,13 @@ impl<'a> Control<'a> {
             }
         }
     }
+}
 
+impl<'a> Control<'a> {
     fn send(&mut self) {
-        let mut cb = unsafe { CONTROL_BLOCK.assume_init_mut() };
+        let cb = &mut *self.cb;
 
-        let mut msg = &mut cb.ctrl_msgs[0];
+        let msg = &mut cb.ctrl_msgs[0];
         msg.channel = 1; // ctrl
 
         let preamble = (self.tx_seq_no as u32) << 16 | 0x01;
@@ -191,9 +289,9 @@ impl<'a> Control<'a> {
     }
 
     pub fn send_at(&mut self, cmd: &mut [u8]) {
-        let mut cb = unsafe { CONTROL_BLOCK.assume_init_mut() };
+        let cb = &mut *self.cb;
 
-        let mut msg = &mut cb.data_msgs[0];
+        let msg = &mut cb.data_msgs[0];
         msg.channel = 2; // data
         msg.id = 0x0001_0003; // AT CMD
         msg.param_len = 4;
@@ -217,13 +315,6 @@ impl<'a> Control<'a> {
         ipc.tasks_send[1].write(|w| unsafe { w.bits(1) });
     }
 }
-
-static mut CONTROL_BLOCK: MaybeUninit<ControlBlock> = MaybeUninit::uninit();
-
-const RX_SIZE: usize = 4096;
-const TX_SIZE: usize = 4096;
-static mut RX: [u8; RX_SIZE] = [0; RX_SIZE];
-static mut TX: [u8; TX_SIZE] = [0; RX_SIZE];
 
 #[repr(C)]
 struct ControlBlock {
